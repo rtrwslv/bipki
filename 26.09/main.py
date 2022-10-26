5486bf612308d47a25e928d81239f04c5b92e9c0
rtrwslv <yarsl.filatov@gmail.com>
2022-10-26 14:31:40 +0300

    nu tipo da

--- 26.09/main.py
+++ 26.09/main.py
@@ -1,3 +1,17 @@
+import time
+
+def time_of_function(function, function2):
+    def wrapped(string):
+        start_time = time.perf_counter_ns()
+        function(string)
+        res = time.perf_counter_ns() - start_time
+        start_time = time.perf_counter_ns()
+        function2(string)
+        res2 = time.perf_counter_ns() - start_time
+        if res < res2: print(res)
+        else: print(res2)
+    return wrapped
+
 def counter(string):
     st = string.lower()
     res = 0
@@ -5,6 +19,7 @@ def counter(string):
         if st.count(i) > 1:
             res += 1
     return(res)
+
 
 def counter_2(string):
     repeating_letters = []
@@ -16,4 +31,6 @@ def counter_2(string):
             repeating_letters.append(letter)
     return(res)
 
-print(counter("aaBbcc"), counter_2("aaBbcc"))
+res = time_of_function(counter, counter_2)
+
+res("aabbcc")
